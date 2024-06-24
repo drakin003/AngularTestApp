4424e84f04fceed3545e52424536773767123c23
Syed Ali Aun <aas745@hotmail.com>
2024-06-25 04:38:51 +0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { AppComponent } from './app.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'calculator', component: CalculatorComponent},
+  { path: 'contact', component: ContactComponent }
+];
---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  email = 'aas745@hotmail.com';
+  phoneNumber = '+92 305 2121738';
+
+}
