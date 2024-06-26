1f3e01175806acccc92bde1a942728616498bf3a
Syed Ali Aun <aas745@hotmail.com>
2024-06-26 20:18:01 +0500

    add caluatator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { MessageService } from './services/message.service';
 import { PrComponent } from './components/pr/pr.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule,NavbarComponent,FooterComponent,ContactComponent, PrComponent ],
+  imports: [RouterLink,RouterOutlet, CommonModule,NavbarComponent,FooterComponent,ContactComponent, PrComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MyPortfolio';
-  msg: string = "";
-
-  constructor(private ms: MessageService){
-    this.msg = ms.showmessage();
-  }
-
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
+import { PrComponent } from './components/pr/pr.component';
 
 export const routes: Routes = [
   { path: 'home', component: AppComponent },
   { path: 'calculator', component: CalculatorComponent},
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'pr', component: PrComponent }
 ];
