1f0461082d2d957a80550fd7caecb2ea76d5d27d
Syed Ali Aun <aas745@hotmail.com>
2024-07-05 17:23:43 +0500

    add page not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { CalculatorComponent } from './components/calculator/calculator.componen
 import { PrComponent } from './components/pr/pr.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { HomeComponent } from './components/home/home.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'calculator', component: CalculatorComponent},
   { path: 'contact', component: ContactComponent },
   { path: 'api', component: PrComponent },
-  { path: 'login', component: FormsComponent }
+  { path: 'login', component: FormsComponent },
+  { path: '**', component: PagenotfoundComponent }
 ];
