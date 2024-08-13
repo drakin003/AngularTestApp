c5dbe96b4fb1b98398b8a2ca783ffd6ab13b9efc
Syed Ali Aun <aas745@hotmail.com>
2024-08-13 05:02:25 +0500

    improve weather UI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient() ,provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), 
+  providers: [provideHttpClient(withFetch()) ,provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), 
     importProvidersFrom(FontAwesomeModule), {provide: LocationStrategy, useClass: PathLocationStrategy}]
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  darkMode: 'class',
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
